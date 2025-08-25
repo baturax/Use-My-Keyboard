1119e5ad4da9e698a8f91df4657c26ad1d6b0c73
Baturax <bai@baturax.com>
2025-08-25 21:20:48 +0300

    first commit

---
+++ src/client/java/user/my/keyboard/UseMyKeyboardClient.java
@@ -0,0 +1,10 @@
+package user.my.keyboard;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UseMyKeyboardClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
