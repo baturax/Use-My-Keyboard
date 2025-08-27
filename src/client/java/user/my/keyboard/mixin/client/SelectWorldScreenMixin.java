242e54ce462240a0d5064a60a80fce3ce1e5cc68
Baturax <bai@baturax.com>
2025-08-28 01:18:01 +0300

    change mixin name

--- src/client/java/user/my/keyboard/mixin/client/SelectWorldScreenMixin.java
+++ src/client/java/user/my/keyboard/mixin/client/SelectWorldScreenMixin.java
@@ -6,13 +6,17 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
 import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 import net.minecraft.client.gui.screen.world.WorldListWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 import static user.my.keyboard.Utilities.changeText;
 import static user.my.keyboard.Utilities.isKeyPressed;
@@ -25,6 +29,14 @@ public class SinglePlayerScreenMixin {
     @Final
     @Shadow
     protected Screen parent;
+
+    @Shadow
+    protected TextFieldWidget searchBox;
+
+    @Unique
+    public TextFieldWidget getSearchBox() {
+        return searchBox;
+    }
 
     @Inject(method = "init", at = @At("TAIL"))
     private void init(CallbackInfo ci) {
@@ -55,6 +67,27 @@ public class SinglePlayerScreenMixin {
         if (isKeyPressed(GLFW.GLFW_KEY_B)) {
             client.setScreen(parent);
         }
+        if (isKeyPressed(GLFW.GLFW_KEY_S)) {
+            searchBox.setFocused(true);
+        }
+
+        // Select
+        if (isKeyPressed(GLFW.GLFW_KEY_1)) selectWorld(0);
+        if (isKeyPressed(GLFW.GLFW_KEY_2)) selectWorld(1);
+        if (isKeyPressed(GLFW.GLFW_KEY_3)) selectWorld(2);
+        if (isKeyPressed(GLFW.GLFW_KEY_1)) selectWorld(3);
+        if (isKeyPressed(GLFW.GLFW_KEY_2)) selectWorld(4);
+        if (isKeyPressed(GLFW.GLFW_KEY_3)) selectWorld(5);
+        if (isKeyPressed(GLFW.GLFW_KEY_1)) selectWorld(6);
+        if (isKeyPressed(GLFW.GLFW_KEY_2)) selectWorld(7);
+        if (isKeyPressed(GLFW.GLFW_KEY_3)) selectWorld(8);
+        if (isKeyPressed(GLFW.GLFW_KEY_1)) selectWorld(9);
+    }
+
+    @Unique
+    private void selectWorld(int worldnum) {
+        List<WorldListWidget.Entry> worlds = levelList.children();
+        if (worlds.size() > worldnum) levelList.setSelected(worlds.get(worldnum));
     }
 
     @Inject(method = "setInitialFocus", at = @At("HEAD"), cancellable = true)
